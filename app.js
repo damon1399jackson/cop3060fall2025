5d9fefcd2863d84c4c6bf9ae1976bf6fd011759e
damon1399jackson <123121660+damon1399jackson@users.noreply.github.com>
2025-10-07 02:51:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ let c = true;
 let x = null;
 let y = ["1","2","3"];
 let z = {type: "letter", case: "lowercase", position: 26};
+
+if (document.getElementById("age") >= 18) {
+  console.out("Adult");
+}
+else if ((document.getElementById("age") > 18) && (document.getElementById("age") < 50)){
+  console.out("Middle Aged");
+}
+else
+  console.out("Old Aged");
+
