f96a0505ffc65191902ee95c41e500ba3d0e03c5
damon1399jackson <123121660+damon1399jackson@users.noreply.github.com>
2025-10-10 22:56:57 -0400

    Update app.js
    added function prototypes to be implemented after html code is altered for listeners.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log("Welcome!");
+console.log("Repository: https://github.com/damon1399jackson/cop3060fall2025.git");
+
+let age = 20, agestr = "20", c = true, d = [], e = null;
+const person = {};
+
+age++; //arithmetic operator demonstration (incrementation).
+console.log(age === agestr); //strict comparison operator demonstration. It will display false b/c the variables have data of different types.
+if (age != agestr) //logical operator demonstration.
+  console.log("Numbers and strings are not equal.");
+
+              
+function getName() {
+
+}
+
+function changeName() { 
+
+}
+
+function checkStatus() {
+
+}
